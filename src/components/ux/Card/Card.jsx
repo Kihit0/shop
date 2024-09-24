ab3c7d7a133b78a174c2e10a6bea7be195b26529
hwaiteu <benner.andrey.loon@gmail.com>
2024-09-24 23:22:11 +0300

    card styles and card list styles

---
+++ src/components/ux/Card/Card.jsx
@@ -0,0 +1,65 @@
+import classNames from 'classnames/bind';
+import styles from './style.module.scss';
+import Button from '../../ui/Button/Button';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+const Card = (props) => {
+  const { v = 'v2', cardData } = props;
+
+  const minimalCard = ({ image, title, id }) => {
+    return (
+      <>
+        <div {...{ className: cx(styles.card__img, 'card__img-minimal') }}>
+          <img {...{ src: image, alt: `card image ${id}` }} />
+        </div>
+        <h2 {...{ className: cx(styles.card__title, 'card__title-minimal') }}>{title}</h2>
+      </>
+    );
+  };
+
+  return (
+    <div {...{ className: styles.card }}>
+      <div {...{ className: styles.card__wrapper }}>
+        <div
+          {...{
+            className: cx(styles.card__content, {
+              'card__content-v1': v === 'v1',
+              'card__content-minimal': v === 'minimal',
+              'card__content-row': v === 'v2' && cardData.id % 2 === 0,
+            }),
+          }}
+        >
+          {v === 'minimal' ? (
+            minimalCard(cardData)
+          ) : (
+            <>
+              <div {...{ className: cx(styles.card__img, { 'card__img-v1': v === 'v1' }) }}>
+                <img {...{ src: cardData.image, alt: `card image S${cardData.id}` }} />
+              </div>
+              <div {...{ className: cx(styles.card__info, { 'card__info-v1': v === 'v1' }) }}>
+                <h2 {...{ className: cx(styles.card__title, { 'card__title-v1': v === 'v1' }) }}>{cardData.title}</h2>
+                {cardData?.price && <p {...{ className: cx(styles.card__price) }}>{cardData.price}</p>}
+                <p {...{ className: cx(styles.card__subtitle, { 'card__subtitle-v1': v === 'v1' }) }}>
+                  {cardData.subtitle}
+                </p>
+                {v === 'v2' ? (
+                  <Link {...{ className: styles.card__link, to: cardData?.href || '/shop' }}>
+                    {cardData?.btnContent || 'Shop'}
+                  </Link>
+                ) : (
+                  <div {...{ className: cx(styles.card__btn) }}>
+                    <Button>Add to card</Button>
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/mock/slider.js
@@ -0,0 +1,14 @@
+export const slider = [
+  {
+    image: 'src/assets/images/header-slider.jpg',
+  },
+  {
+    image: 'src/assets/images/header-slider1.jpg',
+  },
+  {
+    image: 'src/assets/images/header-slider2.jpg',
+  },
+  {
+    image: 'src/assets/images/header-slider3.jpg',
+  },
+];
