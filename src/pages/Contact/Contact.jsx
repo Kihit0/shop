bc20158c518c2d3560c31452129bd146d615c265
hwaiteu <benner.andrey.loon@gmail.com>
2024-10-19 21:50:33 +0300

    Feature: adding message for slider

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,18 @@
 import Slider from '../../components/ux/Slider/Slider';
+import Logo from '../../components/ux/Logo/Logo';
+import Links from '../../components/ux/Links/Links';
+import FacebookIcon from '../../components/ux/Icons/Socials/FacebookIcon';
+import InstagramIcon from '../../components/ux/Icons/Socials/InstagramIcon';
+import LinkedInIcon from '../../components/ux/Icons/Socials/LinkedInIcon';
+import TwitterIcon from '../../components/ux/Icons/Socials/TwitterIcon';
+import YoutubeIcon from '../../components/ux/Icons/Socials/YoutubeIcon';
+import { Link } from 'react-router-dom';
 import Title from '../../components/ux/Title/Title';
 import Map from '../../components/ux/Map/Map';
 import styles from './style.module.scss';
 import classNames from 'classnames/bind';
 import { slider } from '../../mock/slider';
+import { BASE_ROUTING } from '../../constants/index';
 
 const cx = classNames.bind(styles);
 
@@ -31,14 +40,63 @@ const CONTACT = [
   },
 ];
 
+const SOCIALS = [
+  {
+    href: 'https://facebook.com',
+    content: <FacebookIcon />,
+  },
+  {
+    href: 'https://x.com',
+    content: <TwitterIcon />,
+  },
+  {
+    href: 'https://instagram.com',
+    content: <InstagramIcon />,
+  },
+  {
+    href: 'https://linkedin.com',
+    content: <LinkedInIcon />,
+  },
+  {
+    href: 'https://youtube.com',
+    content: <YoutubeIcon />,
+  },
+];
+
 const Contact = () => {
+  const handleClickLink = (e) => {
+    e.preventDefault();
+    console.log(e);
+    window.open();
+  };
+
   return (
     <div {...{ className: styles.contact }}>
       <div>
         <section>
           <Slider {...{ sliderItems: slider }}>
-            <div {...{ className: styles.slider }}>
-              <div {...{ className: cx('container') }}>User</div>
+            <div {...{ className: styles.slider__message }}>
+              <div {...{ className: cx(styles.slider__message_wrapper, 'container') }}>
+                <div {...{ className: styles.slider__message_content }}>
+                  <div {...{ className: styles.slider__logo }}>
+                    <Link {...{ to: `${BASE_ROUTING}` }}>
+                      <Logo {...{ color: 'var(--color-antique-brass)' }} />
+                    </Link>
+                  </div>
+                  <h1 {...{ className: styles.slider__title }}>CONTACT US</h1>
+                  <div {...{ className: styles.slider__btn }}>
+                    <h3>Follow us on social media</h3>
+                    <Links
+                      {...{
+                        links: SOCIALS,
+                        onClick: handleClickLink,
+                        classNameList: styles.slider__list,
+                        classNameLink: styles.slider__social,
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </Slider>
         </section>
